79d673d117183bc45cd0e8c62b5d1f11b72c5a7b
Takefumi MIYOSHI <miyo@wasamon.net>
2017-01-07 16:35:41 +0900

    started to implement HumanGUIPlayer, which is a GUI for human player

--- src/main/java/net/wasamon/geister/player/HumanPlayer.java
+++ src/main/java/net/wasamon/geister/player/HumanPlayer.java
@@ -5,33 +5,34 @@ import net.wasamon.geister.server.*;
 import java.util.*;
 import java.io.*;
 
-public class HumanPlayer extends BasePlayer{
+public class HumanPlayer extends BasePlayer {
 
-    public String readLine() throws IOException{
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	return br.readLine();
+    public String readLine() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        return br.readLine();
     }
 
-    public static void main(String[] args) throws Exception{
-	HumanPlayer p = new HumanPlayer();
-	p.init(args[0], Integer.parseInt(args[1]));
-	System.out.println("red ghosts? (ex. BCDE)");
-	System.out.println(p.setRedItems(p.readLine()));
-	p.printBoard();
-	
-	GAME_LOOP: while(true){	    
-	    if(p.isEnded() == true) break GAME_LOOP;
-	    System.out.println("move ? (ex. A,N)");
-	    System.out.println(p.move(p.readLine()));
-	    p.printBoard();
-	}
-	if(p.isWinner()){
-	    System.out.println("won");
-	}else if(p.isLoser()){
-	    System.out.println("lost");
-	}else if(p.isDraw()){
-	    System.out.println("draw");
-	}
+    public static void main(String[] args) throws Exception {
+        HumanPlayer p = new HumanPlayer();
+        p.init(args[0], Integer.parseInt(args[1]));
+        System.out.println("red ghosts? (ex. BCDE)");
+        System.out.println(p.setRedItems(p.readLine()));
+        p.printBoard();
+
+        GAME_LOOP: while (true) {
+            if (p.isEnded() == true)
+                break GAME_LOOP;
+            System.out.println("move ? (ex. A,N)");
+            System.out.println(p.move(p.readLine()));
+            p.printBoard();
+        }
+        if (p.isWinner()) {
+            System.out.println("won");
+        } else if (p.isLoser()) {
+            System.out.println("lost");
+        } else if (p.isDraw()) {
+            System.out.println("draw");
+        }
     }
 
 }
