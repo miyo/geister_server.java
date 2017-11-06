7e2eb61fb6a62dd5fa3ec250d7c55f52d877359b
Takefumi MIYOSHI <miyo@wasamon.net>
2017-11-06 11:06:50 +0900

    (#5) added timeout mechanism

--- src/main/java/net/wasamon/geister/server/TCPServerWithBlock.java
+++ src/main/java/net/wasamon/geister/server/TCPServerWithBlock.java
@@ -25,8 +25,18 @@ public class TCPServerWithBlock {
 	
 	private Object lock = new Object();
 
+	private int waitTime = 10;
+
 	public TCPServerWithBlock(GameServer server){
 		this.server = server;
+	}
+
+	public void setWaitTime(int v){
+		waitTime = v;
+	}
+	
+	public int getWaitTime(){
+		return waitTime;
 	}
 
 	public void start() throws IOException{
@@ -42,6 +52,7 @@ public class TCPServerWithBlock {
 		}
 	}
 
+	PlayerTimer timer;
 
 	class ServerThread extends Thread{
 
@@ -76,7 +87,7 @@ public class TCPServerWithBlock {
 		}
 
 		synchronized private void send(SocketChannel ch, String msg) throws IOException {
-			// should use OP_WRITE and check writable
+			if(!ch.isConnected()) return;
 			ByteBuffer bb = ByteBuffer.wrap(msg.getBytes());
 			int len = 0;
 			do {
@@ -151,7 +162,10 @@ public class TCPServerWithBlock {
 		}
 
 		private void close() throws IOException {
-			if(ch != null){
+			if(timer != null){
+				timer.terminate();
+			}
+			if(ch != null && ch.isConnected()){
 				ch.shutdownOutput();
 				ch.close();
 			}
@@ -159,7 +173,7 @@ public class TCPServerWithBlock {
 				server.close();
 			}
 		}
-		
+				
 		private boolean action(SocketChannel chan, String str, int pid) throws IOException {
 			boolean result = true;
 			String lastTakenItemColor = "";
@@ -180,8 +194,13 @@ public class TCPServerWithBlock {
 			String stateLabel = "MOV:";
 			
 			if (result) {
-				System.out.println("send: OK");
+				System.out.println("["+pid+"]"+"send: OK");
 				send(chan, String.format("OK%s\r\n", lastTakenItemColor));
+				System.out.println(timer);
+				if(timer != null){
+					System.out.println("timer terminate = " + pid);
+					timer.terminate();
+				}
 			}else{
 				System.out.println("send: NG");
 				send(chan, "NG \r\n");
@@ -190,10 +209,14 @@ public class TCPServerWithBlock {
 			if (result && game.getState() == GameServer.STATE.WAIT_FOR_PLAYER_0) {
 				System.out.println("MOV?" + game.getEncodedBoard(0));
 				send(players[0].ch, "MOV?" + game.getEncodedBoard(0) + "\r\n");
+				timer = new PlayerTimer(game, players, waitTime, 0);
+				timer.start();
 			}
 			if (result && game.getState() == GameServer.STATE.WAIT_FOR_PLAYER_1) {
 				System.out.println("MOV?" + game.getEncodedBoard(1));
 				send(players[1].ch, "MOV?" + game.getEncodedBoard(1) + "\r\n");
+				timer = new PlayerTimer(game, players, waitTime, 1);
+				timer.start();
 			}
 			if (game.getState() == GameServer.STATE.GAME_END) {
 				int winner = game.getWinner();
@@ -226,8 +249,66 @@ public class TCPServerWithBlock {
 				send(players[winner].ch, "WON:" + game.getEncodedBoard(winner) + "\r\n");
 			}
 		}
-		
+
 	}
+
+	class PlayerTimer extends Thread{
+		private final GameServer game;
+		private final ServerThread[] players;
+		private final int waitTime;
+		private final int player;
+		
+		private boolean runFlag = false;
+		private boolean timeout = false;
+
+		public PlayerTimer(GameServer game, ServerThread[] players, int waitTime, int player){
+			this.game = game;
+			this.waitTime = waitTime;
+			this.players = players;
+			this.player = player;
+			this.runFlag = false;
+			this.timeout = false;
+		}
+
+		public void terminate(){
+			runFlag = false;
+		}
+
+		public void run(){
+			System.out.println("PlayerTimer(" + player + "): timeout=" + waitTime);
+			runFlag = true;
+			timeout = false;
+			int t = 0;
+			while(runFlag){
+				try{
+					Thread.sleep(1000);
+					if(t >= waitTime){ // timeout
+						runFlag = false;
+						timeout = true;
+					}
+					t++;
+				}catch(InterruptedException e){
+					e.printStackTrace();
+				}
+			}
+			if(timeout){
+				int loser = player;
+				int winner = player == 0 ? 1 : 0;
+				System.out.println("Timeout: winner=" + winner + ", loser=" + loser);
+				try{
+					System.out.println("WON:" + game.getEncodedBoard(winner));
+					players[winner].send(players[winner].ch, "WON:" + game.getEncodedBoard(winner) + "\r\n");
+					System.out.println("LST:" + game.getEncodedBoard(loser));
+					players[loser].send(players[loser].ch, "LST:" + game.getEncodedBoard(loser) + "\r\n");
+					players[0].close(); // force stop
+					players[1].close(); // force stop
+				}catch(IOException e){
+				}
+			}
+		}
+			
+	}
+
 	
 	private void close() throws IOException {
 		players[0].close();
@@ -239,6 +320,10 @@ public class TCPServerWithBlock {
 		GetOpt opt = new GetOpt("", "no_ng_terminate", args);
 		boolean ng_terminate = !opt.flag("no_ng_terminate");
 		TCPServerWithBlock s = new TCPServerWithBlock(new GameServer(ng_terminate));
+		if(opt.flag("timeout")){
+			s.setWaitTime(Integer.parseInt(opt.getValue("timeout")));
+		}
+		System.out.println("Timeout = " + s.getWaitTime() + " sec.");
 		s.webSocketServer.start();
 		while(true){
             s.server.init();
